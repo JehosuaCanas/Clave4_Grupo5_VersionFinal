2d0b4da6a1ca26b3899c90688d40daf89e35d01f
Anthony Servellón <anthonyservellon4@gmail.com>
2024-10-16 14:18:31 -0600

    Formulariode eventos especiales, con codigo y controles agregados y programados correctamente

--- EventosEspeciales.Designer.cs
+++ EventosEspeciales.Designer.cs
@@ -29,12 +29,121 @@ namespace Clave5_Grupo4
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnCrearEventosEspecial = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPedidoId = new System.Windows.Forms.TextBox();
+            this.txtMontoMaximo = new System.Windows.Forms.TextBox();
+            this.txtMontoMinimo = new System.Windows.Forms.TextBox();
+            this.txtNombreEvento = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCrearEventosEspecial
+            // 
+            this.btnCrearEventosEspecial.Location = new System.Drawing.Point(204, 245);
+            this.btnCrearEventosEspecial.Name = "btnCrearEventosEspecial";
+            this.btnCrearEventosEspecial.Size = new System.Drawing.Size(114, 23);
+            this.btnCrearEventosEspecial.TabIndex = 17;
+            this.btnCrearEventosEspecial.Text = "Crear Evento";
+            this.btnCrearEventosEspecial.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(93, 188);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Id Pedido";
+            // 
+            // txtPedidoId
+            // 
+            this.txtPedidoId.Location = new System.Drawing.Point(234, 188);
+            this.txtPedidoId.Name = "txtPedidoId";
+            this.txtPedidoId.Size = new System.Drawing.Size(100, 20);
+            this.txtPedidoId.TabIndex = 15;
+            // 
+            // txtMontoMaximo
+            // 
+            this.txtMontoMaximo.Location = new System.Drawing.Point(234, 145);
+            this.txtMontoMaximo.Name = "txtMontoMaximo";
+            this.txtMontoMaximo.Size = new System.Drawing.Size(100, 20);
+            this.txtMontoMaximo.TabIndex = 14;
+            // 
+            // txtMontoMinimo
+            // 
+            this.txtMontoMinimo.Location = new System.Drawing.Point(234, 102);
+            this.txtMontoMinimo.Name = "txtMontoMinimo";
+            this.txtMontoMinimo.Size = new System.Drawing.Size(100, 20);
+            this.txtMontoMinimo.TabIndex = 13;
+            // 
+            // txtNombreEvento
+            // 
+            this.txtNombreEvento.Location = new System.Drawing.Point(234, 58);
+            this.txtNombreEvento.Name = "txtNombreEvento";
+            this.txtNombreEvento.Size = new System.Drawing.Size(100, 20);
+            this.txtNombreEvento.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(93, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Monto Maximo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Monto Minimo";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Nombre del Evento";
+            // 
+            // EventosEspeciales
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCrearEventosEspecial);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPedidoId);
+            this.Controls.Add(this.txtMontoMaximo);
+            this.Controls.Add(this.txtMontoMinimo);
+            this.Controls.Add(this.txtNombreEvento);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "EventosEspeciales";
             this.Text = "EventosEspeciales";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCrearEventosEspecial;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPedidoId;
+        private System.Windows.Forms.TextBox txtMontoMaximo;
+        private System.Windows.Forms.TextBox txtMontoMinimo;
+        private System.Windows.Forms.TextBox txtNombreEvento;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
--- EventosEspeciales.cs
+++ EventosEspeciales.cs
@@ -1,20 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Clave5_Grupo4
 {
     public partial class EventosEspeciales : Form
     {
+        private readonly EventosEspeciales eventosEspeciales; // Instancia de la clase EventosEspeciales
         public EventosEspeciales()
         {
             InitializeComponent();
+            var db = new Conexion(); // Crear la instancia de conexión
+            eventosEspeciales = new EventosEspeciales(db); // Inicializar EventosEspeciales con la conexión
+        }
+        private void btnCrearEventosEspecial_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int pedidoId = Convert.ToInt32(txtPedidoId.Text);
+                string nombreEvento = txtNombreEvento.Text;
+                decimal montoMinimo = Convert.ToDecimal(txtMontoMinimo.Text);
+                decimal montoMaximo = Convert.ToDecimal(txtMontoMaximo.Text);
+
+                // Llamar al método para crear el evento especial
+                eventosEspeciales.CrearPedidoEventoEspecial(pedidoId, nombreEvento, montoMinimo, montoMaximo);
+                MessageBox.Show("Evento especial creado exitosamente");
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Error de formato: Asegúrate de que los campos sean válidos.\n" + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ocurrió un error: {ex.Message}");
+            }
+
+            LimpiarFormulario();
+        }
+
+        private void LimpiarFormulario()
+        {
+            txtPedidoId.Clear();
+            txtNombreEvento.Clear();
+            txtMontoMaximo.Clear();
+            txtMontoMinimo.Clear();
+            txtNombreEvento.Focus();
         }
     }
 }
+    
