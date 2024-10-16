0946630194aad7435d51a39d92e80e0481108963
Anthony Servellón <anthonyservellon4@gmail.com>
2024-10-16 14:54:54 -0600

    Formulario para seleccionar el Local, con los cotroles correctamente programados

---
+++ LocalesForm.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace Clave5_Grupo4
+{
+    partial class LocalesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstLocales = new System.Windows.Forms.ListBox();
+            this.btnConsultarLocales = new System.Windows.Forms.Button();
+            this.btnEliminarLocal = new System.Windows.Forms.Button();
+            this.btnModificarLocal = new System.Windows.Forms.Button();
+            this.btnCrearLocal = new System.Windows.Forms.Button();
+            this.txtUbicacionLocal = new System.Windows.Forms.TextBox();
+            this.txtNombreLocal = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstLocales
+            // 
+            this.lstLocales.FormattingEnabled = true;
+            this.lstLocales.Location = new System.Drawing.Point(78, 205);
+            this.lstLocales.Name = "lstLocales";
+            this.lstLocales.Size = new System.Drawing.Size(443, 160);
+            this.lstLocales.TabIndex = 17;
+            // 
+            // btnConsultarLocales
+            // 
+            this.btnConsultarLocales.Location = new System.Drawing.Point(429, 151);
+            this.btnConsultarLocales.Name = "btnConsultarLocales";
+            this.btnConsultarLocales.Size = new System.Drawing.Size(106, 23);
+            this.btnConsultarLocales.TabIndex = 16;
+            this.btnConsultarLocales.Text = "Consultar Locales";
+            this.btnConsultarLocales.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminarLocal
+            // 
+            this.btnEliminarLocal.Location = new System.Drawing.Point(311, 151);
+            this.btnEliminarLocal.Name = "btnEliminarLocal";
+            this.btnEliminarLocal.Size = new System.Drawing.Size(93, 23);
+            this.btnEliminarLocal.TabIndex = 15;
+            this.btnEliminarLocal.Text = "Eliminar Local";
+            this.btnEliminarLocal.UseVisualStyleBackColor = true;
+            // 
+            // btnModificarLocal
+            // 
+            this.btnModificarLocal.Location = new System.Drawing.Point(200, 151);
+            this.btnModificarLocal.Name = "btnModificarLocal";
+            this.btnModificarLocal.Size = new System.Drawing.Size(105, 23);
+            this.btnModificarLocal.TabIndex = 14;
+            this.btnModificarLocal.Text = "Modificar Local";
+            this.btnModificarLocal.UseVisualStyleBackColor = true;
+            // 
+            // btnCrearLocal
+            // 
+            this.btnCrearLocal.Location = new System.Drawing.Point(78, 151);
+            this.btnCrearLocal.Name = "btnCrearLocal";
+            this.btnCrearLocal.Size = new System.Drawing.Size(102, 23);
+            this.btnCrearLocal.TabIndex = 13;
+            this.btnCrearLocal.Text = "Agregar Local";
+            this.btnCrearLocal.UseVisualStyleBackColor = true;
+            // 
+            // txtUbicacionLocal
+            // 
+            this.txtUbicacionLocal.Location = new System.Drawing.Point(217, 84);
+            this.txtUbicacionLocal.Name = "txtUbicacionLocal";
+            this.txtUbicacionLocal.Size = new System.Drawing.Size(100, 20);
+            this.txtUbicacionLocal.TabIndex = 12;
+            // 
+            // txtNombreLocal
+            // 
+            this.txtNombreLocal.Location = new System.Drawing.Point(217, 42);
+            this.txtNombreLocal.Name = "txtNombreLocal";
+            this.txtNombreLocal.Size = new System.Drawing.Size(100, 20);
+            this.txtNombreLocal.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(107, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Ubicacion";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(107, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Nombre Local";
+            // 
+            // LocalesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstLocales);
+            this.Controls.Add(this.btnConsultarLocales);
+            this.Controls.Add(this.btnEliminarLocal);
+            this.Controls.Add(this.btnModificarLocal);
+            this.Controls.Add(this.btnCrearLocal);
+            this.Controls.Add(this.txtUbicacionLocal);
+            this.Controls.Add(this.txtNombreLocal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "LocalesForm";
+            this.Text = "LocalesForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstLocales;
+        private System.Windows.Forms.Button btnConsultarLocales;
+        private System.Windows.Forms.Button btnEliminarLocal;
+        private System.Windows.Forms.Button btnModificarLocal;
+        private System.Windows.Forms.Button btnCrearLocal;
+        private System.Windows.Forms.TextBox txtUbicacionLocal;
+        private System.Windows.Forms.TextBox txtNombreLocal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ LocalesForm.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clave5_Grupo4
+{
+    public partial class LocalesForm : Form
+    {
+        private LocalService localService;
+        private Conexion db;
+        private int localIdSeleccionado = -1;
+
+        public LocalesForm()
+        {
+
+            InitializeComponent();
+            db = new Conexion(); // Crea una nueva instancia de Conexion
+            localService = new LocalService(db); // Pasa la conexión a LocalService
+            lstLocales.SelectedIndexChanged += lstLocales_SelectedIndexChanged;
+        }
+
+        private void btnCrearLocal_Click(object sender, EventArgs e)
+        {
+            localService.CrearLocal(txtNombreLocal.Text, txtUbicacionLocal.Text);
+            MessageBox.Show("Local creado exitosamente");
+            LimpiarCampos();
+        }
+
+
+        private void btnConsultarLocales_Click(object sender, EventArgs e)
+        {
+            var locales = localService.ObtenerLocales();
+            lstLocales.Items.Clear();
+            foreach (var local in locales)
+            {
+                lstLocales.Items.Add($"{local.Id} - {local.Nombre} - {local.Ubicacion}");
+            }
+        }
+
+        private void btnModificarLocal_Click(object sender, EventArgs e)
+        {
+            if (localIdSeleccionado != -1 && ValidarCamposLocal())
+            {
+                localService.ModificarLocal(localIdSeleccionado, txtNombreLocal.Text, txtUbicacionLocal.Text);
+                MessageBox.Show("Local modificado exitosamente");
+                btnConsultarLocales_Click(null, null);
+                LimpiarCampos();
+            }
+            else
+            {
+                MessageBox.Show("Por favor selecciona un local de la lista para modificar.");
+            }
+        }
+
+        private void btnEliminarLocal_Click(object sender, EventArgs e)
+        {
+            if (localIdSeleccionado != -1)
+            {
+                localService.EliminarLocal(localIdSeleccionado);
+                MessageBox.Show("Local eliminado exitosamente");
+                btnConsultarLocales_Click(null, null);
+                LimpiarCampos();
+            }
+            else
+            {
+                MessageBox.Show("Por favor selecciona un local de la lista para eliminar.");
+            }
+        }
+
+        private int ObtenerIdSeleccionado()
+        {
+            if (lstLocales.SelectedItem != null)
+            {
+                string[] partes = lstLocales.SelectedItem.ToString().Split('-');
+                if (int.TryParse(partes[0].Trim(), out int id))
+                {
+                    return id;
+                }
+            }
+            return -1;
+        }
+
+        private void lstLocales_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstLocales.SelectedItem != null)
+            {
+                string selectedItem = lstLocales.SelectedItem.ToString();
+                string[] partes = selectedItem.Split('-');
+                if (partes.Length >= 3)
+                {
+                    try
+                    {
+                        localIdSeleccionado = Convert.ToInt32(partes[0].Trim());
+                        txtNombreLocal.Text = partes[1].Trim();
+                        txtUbicacionLocal.Text = partes[2].Trim();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Ocurrió un error: {ex.Message}");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("El formato del local seleccionado es incorrecto.");
+                }
+            }
+        }
+
+        private void LimpiarCampos()
+        {
+            txtNombreLocal.Clear();
+            txtUbicacionLocal.Clear();
+            txtNombreLocal.Focus();
+            localIdSeleccionado = -1;
+        }
+
+        private bool ValidarCamposLocal()
+        {
+            if (string.IsNullOrWhiteSpace(txtNombreLocal.Text) || string.IsNullOrWhiteSpace(txtUbicacionLocal.Text))
+            {
+                MessageBox.Show("Por favor, completa todos los campos.");
+                return false;
+            }
+            return true;
+        }
+    }
+}
+    
