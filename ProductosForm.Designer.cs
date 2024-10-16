ab26fb3d87ba87faf1cf0a31d1b972a5c4730bfc
Anthony Servellón <anthonyservellon4@gmail.com>
2024-10-16 14:39:51 -0600

    formulario de ProductosForms.cs con controles programados correctamente

---
+++ ProductosForm.Designer.cs
@@ -0,0 +1,220 @@
+﻿
+namespace Clave5_Grupo4
+{
+    partial class ProductosForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbLocales = new System.Windows.Forms.ComboBox();
+            this.lstProductos = new System.Windows.Forms.ListBox();
+            this.btnConsultarProductos = new System.Windows.Forms.Button();
+            this.btnEliminarProducto = new System.Windows.Forms.Button();
+            this.btnModificarProducto = new System.Windows.Forms.Button();
+            this.btnCrearProducto = new System.Windows.Forms.Button();
+            this.cmbTipoProducto = new System.Windows.Forms.ComboBox();
+            this.txtHorarioProducto = new System.Windows.Forms.TextBox();
+            this.txtPrecioProducto = new System.Windows.Forms.TextBox();
+            this.txtNombreProducto = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(475, 91);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 30;
+            this.label5.Text = "Cafetin";
+            // 
+            // cmbLocales
+            // 
+            this.cmbLocales.FormattingEnabled = true;
+            this.cmbLocales.Location = new System.Drawing.Point(475, 112);
+            this.cmbLocales.Name = "cmbLocales";
+            this.cmbLocales.Size = new System.Drawing.Size(121, 21);
+            this.cmbLocales.TabIndex = 29;
+            // 
+            // lstProductos
+            // 
+            this.lstProductos.FormattingEnabled = true;
+            this.lstProductos.Location = new System.Drawing.Point(94, 272);
+            this.lstProductos.Name = "lstProductos";
+            this.lstProductos.Size = new System.Drawing.Size(360, 95);
+            this.lstProductos.TabIndex = 28;
+            // 
+            // btnConsultarProductos
+            // 
+            this.btnConsultarProductos.Location = new System.Drawing.Point(324, 225);
+            this.btnConsultarProductos.Name = "btnConsultarProductos";
+            this.btnConsultarProductos.Size = new System.Drawing.Size(119, 23);
+            this.btnConsultarProductos.TabIndex = 27;
+            this.btnConsultarProductos.Text = "Consultar Productos";
+            this.btnConsultarProductos.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminarProducto
+            // 
+            this.btnEliminarProducto.Location = new System.Drawing.Point(449, 225);
+            this.btnEliminarProducto.Name = "btnEliminarProducto";
+            this.btnEliminarProducto.Size = new System.Drawing.Size(105, 23);
+            this.btnEliminarProducto.TabIndex = 26;
+            this.btnEliminarProducto.Text = "Eliminar Producto";
+            this.btnEliminarProducto.UseVisualStyleBackColor = true;
+            // 
+            // btnModificarProducto
+            // 
+            this.btnModificarProducto.Location = new System.Drawing.Point(192, 225);
+            this.btnModificarProducto.Name = "btnModificarProducto";
+            this.btnModificarProducto.Size = new System.Drawing.Size(116, 23);
+            this.btnModificarProducto.TabIndex = 25;
+            this.btnModificarProducto.Text = "Modificar Producto";
+            this.btnModificarProducto.UseVisualStyleBackColor = true;
+            // 
+            // btnCrearProducto
+            // 
+            this.btnCrearProducto.Location = new System.Drawing.Point(75, 225);
+            this.btnCrearProducto.Name = "btnCrearProducto";
+            this.btnCrearProducto.Size = new System.Drawing.Size(102, 23);
+            this.btnCrearProducto.TabIndex = 24;
+            this.btnCrearProducto.Text = "Agregar Producto";
+            this.btnCrearProducto.UseVisualStyleBackColor = true;
+            // 
+            // cmbTipoProducto
+            // 
+            this.cmbTipoProducto.FormattingEnabled = true;
+            this.cmbTipoProducto.Location = new System.Drawing.Point(252, 130);
+            this.cmbTipoProducto.Name = "cmbTipoProducto";
+            this.cmbTipoProducto.Size = new System.Drawing.Size(121, 21);
+            this.cmbTipoProducto.TabIndex = 23;
+            // 
+            // txtHorarioProducto
+            // 
+            this.txtHorarioProducto.Location = new System.Drawing.Point(252, 168);
+            this.txtHorarioProducto.Name = "txtHorarioProducto";
+            this.txtHorarioProducto.Size = new System.Drawing.Size(121, 20);
+            this.txtHorarioProducto.TabIndex = 22;
+            // 
+            // txtPrecioProducto
+            // 
+            this.txtPrecioProducto.Location = new System.Drawing.Point(252, 92);
+            this.txtPrecioProducto.Name = "txtPrecioProducto";
+            this.txtPrecioProducto.Size = new System.Drawing.Size(121, 20);
+            this.txtPrecioProducto.TabIndex = 21;
+            // 
+            // txtNombreProducto
+            // 
+            this.txtNombreProducto.Location = new System.Drawing.Point(252, 59);
+            this.txtNombreProducto.Name = "txtNombreProducto";
+            this.txtNombreProducto.Size = new System.Drawing.Size(121, 20);
+            this.txtNombreProducto.TabIndex = 20;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(91, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Horario";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(91, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Tipo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(91, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Precio";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(91, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Nombre del Producto";
+            // 
+            // ProductosForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cmbLocales);
+            this.Controls.Add(this.lstProductos);
+            this.Controls.Add(this.btnConsultarProductos);
+            this.Controls.Add(this.btnEliminarProducto);
+            this.Controls.Add(this.btnModificarProducto);
+            this.Controls.Add(this.btnCrearProducto);
+            this.Controls.Add(this.cmbTipoProducto);
+            this.Controls.Add(this.txtHorarioProducto);
+            this.Controls.Add(this.txtPrecioProducto);
+            this.Controls.Add(this.txtNombreProducto);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ProductosForm";
+            this.Text = "ProductosForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbLocales;
+        private System.Windows.Forms.ListBox lstProductos;
+        private System.Windows.Forms.Button btnConsultarProductos;
+        private System.Windows.Forms.Button btnEliminarProducto;
+        private System.Windows.Forms.Button btnModificarProducto;
+        private System.Windows.Forms.Button btnCrearProducto;
+        private System.Windows.Forms.ComboBox cmbTipoProducto;
+        private System.Windows.Forms.TextBox txtHorarioProducto;
+        private System.Windows.Forms.TextBox txtPrecioProducto;
+        private System.Windows.Forms.TextBox txtNombreProducto;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ ProductosForm.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Clave5_Grupo4
+{
+    public partial class ProductosForm : Form
+    {
+        private ProductoService productoService;
+        private LocalService localService;
+        private int productoIdSeleccionado = -1;
+        private Conexion db;
+
+        public ProductosForm()
+        {
+            InitializeComponent();
+        }
+        public ProductosForm()
+        {
+            InitializeComponent();
+            db = new Conexion();  // Crea una nueva instancia de Conexion
+            productoService = new ProductoService(db);
+            localService = new LocalService(db);
+
+
+            cmbTipoProducto.Items.AddRange(new string[] { "comida", "bebida", "antojo" });
+            lstProductos.SelectedIndexChanged += lstProductos_SelectedIndexChanged;
+            CargarLocalesEnComboBox();
+        }
+
+        private void CargarLocalesEnComboBox()
+        {
+            var locales = localService.ObtenerLocalesConId(); // Utiliza LocalService para obtener los locales
+            cmbLocales.DataSource = locales;
+            cmbLocales.DisplayMember = "Nombre";
+            cmbLocales.ValueMember = "Id";
+        }
+
+        private bool EsHorarioValido(string horario)
+        {
+            return TimeSpan.TryParse(horario, out _) || TimeSpan.TryParseExact(horario, "hh\\:mm", null, out _);
+        }
+
+        private void btnCrearProducto_Click(object sender, EventArgs e)
+        {
+            if (!EsHorarioValido(txtHorarioProducto.Text))
+            {
+                MessageBox.Show("Por favor ingresa un horario válido en formato 24 horas (HH:mm).");
+                return;
+            }
+
+            int localId = (int)cmbLocales.SelectedValue;
+            productoService.CrearProducto(
+                txtNombreProducto.Text,
+                Convert.ToDecimal(txtPrecioProducto.Text),
+                cmbTipoProducto.SelectedItem.ToString(),
+                TimeSpan.Parse(txtHorarioProducto.Text),
+                localId
+            );
+
+            MessageBox.Show("Producto creado exitosamente");
+            LimpiarCampos();
+        }
+
+        private void btnConsultarProductos_Click(object sender, EventArgs e)
+        {
+            var productos = productoService.ObtenerProductos((int)cmbLocales.SelectedValue);
+            lstProductos.Items.Clear();
+            foreach (var producto in productos)
+            {
+                lstProductos.Items.Add(producto);
+            }
+        }
+
+        private void btnModificarProducto_Click(object sender, EventArgs e)
+        {
+            if (productoIdSeleccionado != -1 && ValidarCamposProducto())
+            {
+                int localId = (int)cmbLocales.SelectedValue;
+                try
+                {
+                    decimal precioProducto = Convert.ToDecimal(txtPrecioProducto.Text);
+                    TimeSpan horarioProducto;
+
+                    // Intenta parsear en el formato HH:mm:ss primero, si no funciona, intenta con HH:mm
+                    if (!TimeSpan.TryParse(txtHorarioProducto.Text, out horarioProducto))
+                    {
+                        // Si no se puede parsear, intenta con el formato HH:mm
+                        if (!TimeSpan.TryParseExact(txtHorarioProducto.Text, "hh\\:mm", null, out horarioProducto))
+                        {
+                            MessageBox.Show("Formato de horario no válido. Usa HH:mm o HH:mm:ss.");
+                            return;
+                        }
+                    }
+
+                    productoService.ModificarProducto(
+                        productoIdSeleccionado,
+                        txtNombreProducto.Text,
+                        precioProducto,
+                        cmbTipoProducto.SelectedItem.ToString(),
+                        horarioProducto,
+                        localId
+                    );
+
+                    MessageBox.Show("Producto modificado exitosamente");
+                    btnConsultarProductos_Click(null, null);
+                }
+                catch (FormatException ex)
+                {
+                    MessageBox.Show("Error de formato: Asegúrate de que los campos de precio y horario tengan un formato válido.\n" + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor selecciona un producto de la lista para modificar.");
+            }
+
+            LimpiarCampos();
+        }
+
+
+        private void btnEliminarProducto_Click(object sender, EventArgs e)
+        {
+            int productoId = ObtenerIdSeleccionado();
+            if (productoId != -1)
+            {
+                productoService.EliminarProducto(productoId);
+                MessageBox.Show("Producto eliminado exitosamente");
+                btnConsultarProductos_Click(null, null);
+            }
+        }
+
+        private int ObtenerIdSeleccionado()
+        {
+            if (lstProductos.SelectedItem != null)
+            {
+                string[] partes = lstProductos.SelectedItem.ToString().Split('-');
+                return Convert.ToInt32(partes[0].Trim());
+            }
+            return -1;
+        }
+
+        private void lstProductos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstProductos.SelectedItem != null)
+            {
+                string selectedItem = lstProductos.SelectedItem.ToString();
+                string[] partes = selectedItem.Split('-');
+                if (partes.Length >= 6)
+                {
+                    try
+                    {
+                        productoIdSeleccionado = Convert.ToInt32(partes[0].Trim());
+                        txtNombreProducto.Text = partes[1].Trim();
+                        txtPrecioProducto.Text = partes[2].Trim();
+                        cmbTipoProducto.SelectedItem = partes[3].Trim();
+                        txtHorarioProducto.Text = partes[4].Trim();
+
+                        if (int.TryParse(partes[5].Trim(), out int cafetinId))
+                        {
+                            cmbLocales.SelectedValue = cafetinId;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Ocurrió un error: {ex.Message}");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("El formato del producto seleccionado es incorrecto.");
+                }
+            }
+        }
+
+        private void LimpiarCampos()
+        {
+            txtNombreProducto.Clear();
+            txtPrecioProducto.Clear();
+            txtHorarioProducto.Clear();
+            txtNombreProducto.Focus();
+            productoIdSeleccionado = -1;
+        }
+
+        private bool ValidarCamposProducto()
+        {
+            if (string.IsNullOrWhiteSpace(txtNombreProducto.Text) ||
+                string.IsNullOrWhiteSpace(txtPrecioProducto.Text) ||
+                cmbTipoProducto.SelectedItem == null ||
+                string.IsNullOrWhiteSpace(txtHorarioProducto.Text))
+            {
+                MessageBox.Show("Por favor, completa todos los campos.");
+                return false;
+            }
+
+            if (!decimal.TryParse(txtPrecioProducto.Text, out _) || !EsHorarioValido(txtHorarioProducto.Text))
+            {
+                MessageBox.Show("Por favor, ingresa un precio y horario válido.");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
+
+   
