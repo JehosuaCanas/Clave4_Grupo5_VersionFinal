a5e0501b3452f45a82634bb794736ad425966748
Cañas Tijerino Jehosua Abdiel <CT24001@ti.ues.edu.sv>
2024-10-21 17:54:10 -0600

    Agregando de nuevo la clase Conexion

--- Conexion.cs
+++ Conexion.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
 
 namespace Clave5_Grupo4
 {
-    public MySqlConnection Connection { get; }
-
-    class Conexion()
+    public class Conexion
     {
-        Connection = new MySqlConnection("server=localhost;user=root;password=root;database=cafetin_ues");
-        Connection.Open();
+        public MySqlConnection Connection { get; }
+
+        public Conexion()
+        {
+            Connection = new MySqlConnection("server=localhost;user=root;password=root;database=cafetin_ues");
+            Connection.Open();
+        }
     }
 }
