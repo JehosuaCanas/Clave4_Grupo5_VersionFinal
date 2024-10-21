313625da6aec1f50887832ceee253384a42a61e2
Cañas Tijerino Jehosua Abdiel <CT24001@ti.ues.edu.sv>
2024-10-21 17:47:51 -0600

    Añadiendo la clase conexio para poder conectarse a la base de datos y cambiando el nombre de la solucion

---
+++ Conexion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Clave5_Grupo4
+{
+    public MySqlConnection Connection { get; }
+
+    class Conexion()
+    {
+        Connection = new MySqlConnection("server=localhost;user=root;password=root;database=cafetin_ues");
+        Connection.Open();
+    }
+}
