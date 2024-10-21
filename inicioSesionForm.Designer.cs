df166e2f2e64c1f3fa45478e5bec980c8641d75b
Cañas Tijerino Jehosua Abdiel <CT24001@ti.ues.edu.sv>
2024-10-21 17:30:19 -0600

    Agregando componentes y funcionalidad al formulario de inicioSesionForm

--- inicioSesionForm.Designer.cs
+++ inicioSesionForm.Designer.cs
@@ -29,21 +29,95 @@ namespace Clave5_Grupo4
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnIniciarSesion = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtContraseñatxtContraseña = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // btnIniciarSesion
+            // 
+            this.btnIniciarSesion.Location = new System.Drawing.Point(90, 232);
+            this.btnIniciarSesion.Name = "btnIniciarSesion";
+            this.btnIniciarSesion.Size = new System.Drawing.Size(122, 23);
+            this.btnIniciarSesion.TabIndex = 0;
+            this.btnIniciarSesion.Text = "Iniciar Sesion";
+            this.btnIniciarSesion.UseVisualStyleBackColor = true;
+            this.btnIniciarSesion.Click += new System.EventHandler(this.btnIniciarSesion_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtContraseñatxtContraseña);
+            this.groupBox1.Controls.Add(this.btnIniciarSesion);
+            this.groupBox1.Controls.Add(this.txtEmail);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(76, 37);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(313, 327);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Iniciar Sesion";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Contraseña";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(127, 61);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(148, 20);
+            this.txtEmail.TabIndex = 2;
+            // 
+            // txtContraseñatxtContraseña
+            // 
+            this.txtContraseñatxtContraseña.Location = new System.Drawing.Point(127, 100);
+            this.txtContraseñatxtContraseña.Name = "txtContraseñatxtContraseña";
+            this.txtContraseñatxtContraseña.PasswordChar = '*';
+            this.txtContraseñatxtContraseña.Size = new System.Drawing.Size(148, 20);
+            this.txtContraseñatxtContraseña.TabIndex = 3;
+            // 
+            // inicioSesionForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(531, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "inicioSesionForm";
+            this.Text = "Inicio Sesion";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnIniciarSesion;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtContraseñatxtContraseña;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- inicioSesionForm.cs
+++ inicioSesionForm.cs
@@ -12,13 +12,60 @@ namespace Clave5_Grupo4
 {
     public partial class inicioSesionForm : Form
     {
+        // Variable privada para poder tener acceso a los metodos de la base de datos 
+        private Conexion db;
+        private UsuarioService usuarioService;
+
         public inicioSesionForm()
         {
             InitializeComponent();
+            // Se inicializa la variable db
+            db = new Conexion();
+            usuarioService = new UsuarioService(db); // Pasamos la instancia de Database directamente
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
+        }
+
+        private void btnIniciarSesion_Click(object sender, EventArgs e)
+        {
+            string email = txtEmail.Text;
+            string contrasena = txtContraseña.Text;
+
+            // Verifica que los campos de email y contraseña tenga datos
+            if (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(contrasena))
+            {
+                MessageBox.Show("Por favor, ingrese su email y contraseña.");
+                return;
+            }
+
+            // Llama el método ValidarUsuario desde el servicio de usuario
+            var usuario = usuarioService.ValidarUsuario(email, contrasena);
+
+            if (usuario != null)
+            {
+                MessageBox.Show($"Bienvenido, {usuario.Nombre}!");
+
+                // Aquí puedes abrir la ventana correspondiente según el tipo de usuario
+                if (usuario.Tipo == "administrativo")
+                {
+                    MainForm mainForm = new MainForm();
+                    mainForm.Show();
+                }
+                else if (usuario.Tipo == "estudiante" || usuario.Tipo == "docente")
+                {
+                    AdminForm adminForm = new AdminForm();
+                    adminForm.Show();
+                }
+
+                this.Hide(); // Ocultar el formulario de inicio de sesión
+            }
+            else
+            {
+                MessageBox.Show("Email o contraseña incorrectos.");
+            }
 
         }
     }
