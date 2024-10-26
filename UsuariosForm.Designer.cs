90c2e35b6e7b645c43a26abbb1c0fb2de9d8b919
Manuel Alberto Aguirre Cortes <1manuelcaso1@gmail.com>
2024-10-25 21:06:12 -0600

    Trabajando en UsuariosForm

---
+++ UsuariosForm.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace Clave4_Grupo5
+{
+    partial class UsuarioForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstUsuarios = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnEliminarUsuario = new System.Windows.Forms.Button();
+            this.btnModificarUsuario = new System.Windows.Forms.Button();
+            this.btnCrearUsuario = new System.Windows.Forms.Button();
+            this.cmbTipo = new System.Windows.Forms.ComboBox();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.crearUsuarioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.localesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.productosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pedidosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eventosEspecialesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lstUsuarios
+            // 
+            this.lstUsuarios.FormattingEnabled = true;
+            this.lstUsuarios.Location = new System.Drawing.Point(58, 276);
+            this.lstUsuarios.Name = "lstUsuarios";
+            this.lstUsuarios.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lstUsuarios.Size = new System.Drawing.Size(469, 95);
+            this.lstUsuarios.TabIndex = 26;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(304, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 23);
+            this.button1.TabIndex = 25;
+            this.button1.Text = "Consultar Usuario";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminarUsuario
+            // 
+            this.btnEliminarUsuario.Location = new System.Drawing.Point(433, 208);
+            this.btnEliminarUsuario.Name = "btnEliminarUsuario";
+            this.btnEliminarUsuario.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminarUsuario.TabIndex = 24;
+            this.btnEliminarUsuario.Text = "Eliminar Usuario";
+            this.btnEliminarUsuario.UseVisualStyleBackColor = true;
+            // 
+            // btnModificarUsuario
+            // 
+            this.btnModificarUsuario.Location = new System.Drawing.Point(184, 208);
+            this.btnModificarUsuario.Name = "btnModificarUsuario";
+            this.btnModificarUsuario.Size = new System.Drawing.Size(99, 23);
+            this.btnModificarUsuario.TabIndex = 23;
+            this.btnModificarUsuario.Text = "Modificar Usuario";
+            this.btnModificarUsuario.UseVisualStyleBackColor = true;
+            // 
+            // btnCrearUsuario
+            // 
+            this.btnCrearUsuario.Location = new System.Drawing.Point(58, 208);
+            this.btnCrearUsuario.Name = "btnCrearUsuario";
+            this.btnCrearUsuario.Size = new System.Drawing.Size(97, 23);
+            this.btnCrearUsuario.TabIndex = 22;
+            this.btnCrearUsuario.Text = "Crear Usuario";
+            this.btnCrearUsuario.UseVisualStyleBackColor = true;
+            // 
+            // cmbTipo
+            // 
+            this.cmbTipo.FormattingEnabled = true;
+            this.cmbTipo.Location = new System.Drawing.Point(195, 62);
+            this.cmbTipo.Name = "cmbTipo";
+            this.cmbTipo.Size = new System.Drawing.Size(197, 21);
+            this.cmbTipo.TabIndex = 21;
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(195, 136);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(197, 20);
+            this.txtContraseña.TabIndex = 20;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(195, 101);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(197, 20);
+            this.txtEmail.TabIndex = 19;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(195, 21);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(197, 20);
+            this.txtNombre.TabIndex = 18;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(74, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Contraseña";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(74, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Tipo";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(74, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Nombre";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.crearUsuarioToolStripMenuItem,
+            this.localesToolStripMenuItem,
+            this.productosToolStripMenuItem,
+            this.pedidosToolStripMenuItem,
+            this.eventosEspecialesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(540, 24);
+            this.menuStrip1.TabIndex = 27;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // crearUsuarioToolStripMenuItem
+            // 
+            this.crearUsuarioToolStripMenuItem.Name = "crearUsuarioToolStripMenuItem";
+            this.crearUsuarioToolStripMenuItem.Size = new System.Drawing.Size(90, 20);
+            this.crearUsuarioToolStripMenuItem.Text = "Crear Usuario";
+            // 
+            // localesToolStripMenuItem
+            // 
+            this.localesToolStripMenuItem.Name = "localesToolStripMenuItem";
+            this.localesToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.localesToolStripMenuItem.Text = "Locales";
+            // 
+            // productosToolStripMenuItem
+            // 
+            this.productosToolStripMenuItem.Name = "productosToolStripMenuItem";
+            this.productosToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
+            this.productosToolStripMenuItem.Text = "Productos";
+            // 
+            // pedidosToolStripMenuItem
+            // 
+            this.pedidosToolStripMenuItem.Name = "pedidosToolStripMenuItem";
+            this.pedidosToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.pedidosToolStripMenuItem.Text = "Pedidos";
+            // 
+            // eventosEspecialesToolStripMenuItem
+            // 
+            this.eventosEspecialesToolStripMenuItem.Name = "eventosEspecialesToolStripMenuItem";
+            this.eventosEspecialesToolStripMenuItem.Size = new System.Drawing.Size(116, 20);
+            this.eventosEspecialesToolStripMenuItem.Text = "Eventos Especiales";
+            // 
+            // UsuariosForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 382);
+            this.Controls.Add(this.lstUsuarios);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnEliminarUsuario);
+            this.Controls.Add(this.btnModificarUsuario);
+            this.Controls.Add(this.btnCrearUsuario);
+            this.Controls.Add(this.cmbTipo);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "UsuariosForm";
+            this.Text = "UsuariosForm";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstUsuarios;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnEliminarUsuario;
+        private System.Windows.Forms.Button btnModificarUsuario;
+        private System.Windows.Forms.Button btnCrearUsuario;
+        private System.Windows.Forms.ComboBox cmbTipo;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem crearUsuarioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem localesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem productosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pedidosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eventosEspecialesToolStripMenuItem;
+    }
+}
---
+++ UsuariosForm.cs
@@ -0,0 +1,202 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows.Forms;
+
+//namespace Clave5_Grupo4
+//{
+//    public partial class UsuariosForm : Form
+//    {
+//        public UsuariosForm()
+//        {
+//            InitializeComponent();
+//        }
+//    }
+//}
+
+using Clave5_Grupo4;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clave4_Grupo5
+{
+    public partial class UsuarioForm : Form
+    {
+        private Conexion db; // Instancia de la clase Conexion
+        private UsuarioService usuarioService; // Servicio para la gestión de usuarios
+        private int usuarioIdSeleccionado = -1;
+
+        public UsuarioForm()
+        {
+            InitializeComponent();
+            db = new Conexion(); // Inicializa la conexión a la base de datos
+            usuarioService = new UsuarioService(db); // Inicializa el servicio de usuarios
+
+            cmbTipo.Items.AddRange(new string[] { "estudiante", "docente", "administrativo" }); // Opciones del ComboBox
+            lstUsuarios.SelectedIndexChanged += lstUsuarios_SelectedIndexChanged; // Evento para cambiar la selección del ListBox
+        }
+
+        private void btnCrearUsuario_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string tipo = cmbTipo.SelectedItem?.ToString();
+            string email = txtEmail.Text;
+            string contrasena = txtContraseña.Text;
+
+            if (string.IsNullOrWhiteSpace(nombre) || string.IsNullOrWhiteSpace(tipo) || string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(contrasena))
+            {
+                MessageBox.Show("Todos los campos son obligatorios.");
+                return;
+            }
+
+            usuarioService.CrearUsuario(nombre, tipo, email, contrasena);
+            LimpiarCampos();
+            CargarUsuarios(); // Actualiza la lista de usuarios
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CargarUsuarios(); // Carga todos los usuarios
+        }
+
+        private void btnModificarUsuario_Click(object sender, EventArgs e)
+        {
+            if (usuarioIdSeleccionado != -1 && cmbTipo.SelectedItem != null)
+            {
+                string nombre = txtNombre.Text;
+                string tipo = cmbTipo.SelectedItem.ToString();
+                string email = txtEmail.Text;
+
+                usuarioService.ModificarUsuario(usuarioIdSeleccionado, nombre, tipo, email);
+                MessageBox.Show("Usuario modificado exitosamente.");
+                LimpiarCampos();
+                CargarUsuarios(); // Actualiza la lista de usuarios
+            }
+            else
+            {
+                MessageBox.Show("Por favor, seleccione un usuario y un tipo de usuario.");
+            }
+        }
+
+        private void btnEliminarUsuario_Click(object sender, EventArgs e)
+        {
+            if (usuarioIdSeleccionado != -1)
+            {
+                usuarioService.EliminarUsuario(usuarioIdSeleccionado);
+                MessageBox.Show("Usuario eliminado exitosamente.");
+                LimpiarCampos();
+                CargarUsuarios(); // Actualiza la lista de usuarios
+            }
+            else
+            {
+                MessageBox.Show("Por favor, seleccione un usuario para eliminar.");
+            }
+        }
+
+        private void CargarUsuarios()
+        {
+            var usuarios = usuarioService.ObtenerUsuarios();
+            lstUsuarios.Items.Clear();
+
+            foreach (var usuario in usuarios)
+            {
+                // Mostrar el usuario en el formato id - nombre - tipo - email
+                lstUsuarios.Items.Add($"{usuario.Id} - {usuario.Nombre} - {usuario.Tipo} - {usuario.Email}");
+            }
+        }
+
+        private void lstUsuarios_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstUsuarios.SelectedItem != null)
+            {
+                string selectedItem = lstUsuarios.SelectedItem.ToString();
+                string[] partes = selectedItem.Split('-');
+
+                if (partes.Length >= 4)
+                {
+                    usuarioIdSeleccionado = Convert.ToInt32(partes[0].Trim());
+                    txtNombre.Text = partes[1].Trim();
+                    string tipoUsuario = partes[2].Trim();
+                    txtEmail.Text = partes[3].Trim();
+
+                    if (cmbTipo.Items.Contains(tipoUsuario))
+                    {
+                        cmbTipo.SelectedItem = tipoUsuario;
+                    }
+                    else
+                    {
+                        MessageBox.Show("El tipo de usuario no es válido.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("El formato del usuario seleccionado es incorrecto.");
+                }
+            }
+        }
+
+        private void LimpiarCampos()
+        {
+            txtNombre.Clear();
+            txtContraseña.Clear();
+            txtEmail.Clear();
+            cmbTipo.SelectedIndex = -1;
+            usuarioIdSeleccionado = -1;
+        }
+
+        private void cmbTipo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void btnConsultarUsuario_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void crearUsuarioToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            UsuarioForm usuariosForm = new UsuarioForm();
+            usuariosForm.Show();
+            this.Close();
+        }
+
+        private void localesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LocalesForm localesForm = new LocalesForm();
+            localesForm.Show();
+            this.Close();
+        }
+
+        private void productosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ProductosForm productosForm = new ProductosForm();
+            productosForm.Show();
+            this.Close();
+        }
+
+        private void pedidosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            PedidosForm pedidosForm = new PedidosForm();
+            pedidosForm.Show();
+            this.Close();
+        }
+
+        private void eventosEspecialesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            EventosEspecialesForm eventosForm = new EventosEspecialesForm();
+            eventosForm.Show();
+            this.Close();
+        }
+    }
+}
